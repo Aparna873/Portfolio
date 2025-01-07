c0d1e3fe0afd608de7490aab16b4220dd693f1d5
Aparna Shukla <appsworld837@gmail.com>
2025-01-07 22:12:40 +0530

    portfolio

---
+++ src/data.js
@@ -0,0 +1,100 @@
+const projects = [
+  {
+    id: 1,
+    title: "Book Hotel Now",
+    img: "/imgs/projects/BookNow.png",
+    desc: "A Hotel Booking website where anyone can search and book hotels on thier preferred location.",
+    technologies: ["React.js", "Serp API"],
+    github: "https://github.com/Aparna873/Hotel-Booking",
+    live: "https://hotel-booking-nine-lilac.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Tasty Hub",
+    img: "/imgs/projects/TastyHub.png",
+    desc: "A Food Ordering website where people can order food online with responsive UI.",
+    technologies: ["React.js", "Firebase"],
+    github: "https://github.com/Aparna873/fdw",
+    live: "https://fdw.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Expense Expert",
+    img: "/imgs/projects/ExpenseXpert.png",
+    desc: "A web app where you can add, view, edit, delete and track your expenses.",
+    technologies: ["React JS", "Firebase"],
+    github: "https://github.com/ashish12501/ExpensXpert",
+    live: "https://expensxpert.web.app/",
+  },
+  {
+    id: 3,
+    title: "PomoDoro Timer",
+    img: "/imgs/projects/PomoDoro.png",
+    desc: "A pomodoro timer technique used by people for focused and productive work.",
+    technologies: ["React.js"],
+    github: "https://github.com/Aparna873/Pomodoro_App",
+    live: "https://rock-paper-scissor-1tur.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Rock Paper Scissor",
+    img: "/imgs/projects/RockPaperScissor.png",
+    desc: "An game that can be played for fun with the computer.",
+    technologies: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/Aparna873/Rock_Paper_Scissor",
+    live: "https://rock-paper-scissor-tau-lac.vercel.app/",
+  }
+];
+
+const skills = [
+  {
+    icon: "html",
+    title: "HTML",
+  },
+  {
+    icon: "css",
+    title: "CSS",
+  },
+  {
+    icon: "javascript",
+    title: "JavaScript",
+  },
+  {
+    icon: "react",
+    title: "React.js",
+  },
+  {
+    icon: "firebase",
+    title: "Firebase",
+  },
+  {
+    icon: "sass",
+    title: "Sass",
+  },
+  {
+    icon: "tailwind",
+    title: "Tailwind",
+  },
+  {
+    icon: "express",
+    title: "Express",
+  },
+  {
+    icon: "mysql",
+    title: "Mysql",
+  },
+  {
+    icon: "git",
+    title: "Git",
+  },
+  {
+    icon: "figma",
+    title: "Figma",
+  },
+  {
+    icon: "github",
+    title: "Github",
+  },
+];
+
+export const data = { projects, skills };
